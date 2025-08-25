10c097b4f91ecd887683bd89b72b946c4259846d
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-08-25 19:35:42 -0300

    Atualização do WebDesem.jsx

--- src/components/WebSection/WebDesen.jsx
+++ src/components/WebSection/WebDesen.jsx
@@ -3,17 +3,25 @@ import ButtonUni from "../Button/ButtonUniversal";
 import ProgramadorWeb from "../Simbolos/Programador";
 
 function WebDesenTexto() {
-    return (
-        <div className="web-desen place-content-start">
-            <ProgramadorWeb />
-            <h2>Programas customizados que automatizam seu sucesso</h2>
-           
-            <p>Desenvolvemos programas e sistemas sob medida que automatizam processos, eliminam gargalos e maximizam eficiência. Nossa abordagem high-tech transforma complexidade em simplicidade.</p>
-          
-            <ButtonUni>Quero saber mais</ButtonUni>
+  return (
+    <div className="web-desen place-content-start ml-auto justify-self-end px-[390px]">
+      <ProgramadorWeb />
 
-            </div>
-    );  
+      <article className="text-wrap">
+        <h2>Programas customizados</h2>
+        <h2>que automatizam seu</h2>
+        <h2>sucesso</h2>
+      </article>
+
+       <p className="!max-w-[560px] !w-auto !text-left !m-0 mt-2">
+        Desenvolvemos programas e sistemas sob medida que automatizam processos,
+        eliminam gargalos e maximizam eficiência. Nossa abordagem high-tech
+        transforma complexidade em simplicidade.
+      </p>
+        <br></br>
+      <ButtonUni>Quero saber mais</ButtonUni>
+    </div>
+  );
 }
 
 export default WebDesenTexto;
