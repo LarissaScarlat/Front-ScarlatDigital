2de4c29bb9af857bbcfaa4bbea18178338f718f1
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-08-28 16:50:03 -0300

    commit

--- src/components/Questions/Faq.jsx
+++ src/components/Questions/Faq.jsx
@@ -4,20 +4,74 @@ import FaqQuestion from "../Simbolos/FaqQuestion.jsx";
 
 function Faq() {
   return (
-    <div className="w-full pt-20 pb-[100px] px-[390px]">  {/* w-full e sem justify-center */}
+    <div className="faqcontainer pt-20">
       <div className="content">
         <FaqQuestion />
 
-        <article className="text-wrap mb-6">
+        <article className="text-wrap mb-6 w-full  pb-[100px] px-[390px] flex flex-col items-start">
           <h2>Perguntas</h2>
           <h2>frequentes</h2>
         </article>
 
-        {/* Linha do FAQ: texto à esquerda e + à direita */}
-        <div className="w-[600px] ml-auto mr-[390px]">
+        {/* Pergunta n° 1 */}
+        <div className="w-[600px] ml-auto mr-[390px] ">
           <div className="flex items-center justify-between border-b border-neutral-700 py-4">
             <p className="text-base text-neutral-50">
               Qual a diferença do Grupo Scarlat para outras agências?
+            </p>
+            <Plus size={20} className="shrink-0 text-neutral-200" />
+          </div>
+        </div>
+        { /*Pergunta n° 2 */}
+         <div className="w-[600px] ml-auto mr-[390px]">
+          <div className="flex items-center justify-between border-b border-neutral-700 py-4">
+            <p className="text-base text-neutral-50">
+              Vocês trabalham com todos os tipos de marketplace?
+            </p>
+            <Plus size={20} className="shrink-0 text-neutral-200" />
+          </div>
+        </div>
+        { /*Pergunta n° 3 */}
+         <div className="w-[600px] ml-auto mr-[390px]">
+          <div className="flex items-center justify-between border-b border-neutral-700 py-4">
+            <p className="text-base text-neutral-50">
+              Qual o investimento mínimo para começar?
+            </p>
+            <Plus size={20} className="shrink-0 text-neutral-200" />
+          </div>
+        </div>
+        { /*Pergunta n° 4 */}
+         <div className="w-[600px] ml-auto mr-[390px]">
+          <div className="flex items-center justify-between border-b border-neutral-700 py-4">
+            <p className="text-base text-neutral-50">
+              Em quanto tempo vejo os primeiros resultados?
+            </p>
+            <Plus size={20} className="shrink-0 text-neutral-200" />
+          </div>
+        </div>
+        { /*Pergunta n° 5 */}
+         <div className="w-[600px] ml-auto mr-[390px]">
+          <div className="flex items-center justify-between border-b border-neutral-700 py-4">
+            <p className="text-base text-neutral-50">
+              Como funciona o processo de implementação?
+            </p>
+            <Plus size={20} className="shrink-0 text-neutral-200" />
+          </div>
+        </div>
+        { /*Pergunta n° 6 */}
+         <div className="w-[600px] ml-auto mr-[390px]">
+          <div className="flex items-center justify-between border-b border-neutral-700 py-4">
+            <p className="text-base text-neutral-50">
+              Preciso ter conhecimento técnico para trabalhar com vocês?
+            </p>
+            <Plus size={20} className="shrink-0 text-neutral-200" />
+          </div>
+        </div>
+        { /*Pergunta n° 7 */}
+         <div className="w-[600px] ml-auto mr-[390px]">
+          <div className="flex items-center justify-between border-b border-neutral-700 py-4">
+            <p className="text-base text-neutral-50">
+              Qual o nível de suporte oferecido?
             </p>
             <Plus size={20} className="shrink-0 text-neutral-200" />
           </div>
--- src/components/Simbolos/FaqQuestion.jsx
+++ src/components/Simbolos/FaqQuestion.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function FaqQuestion() {
   return (
-    <div class="place-content-start..">
+    <div class="place-content-start.. px-[390px] ">
       <div className="status-container">
           
-          <span className="status-text">FAQ</span>
+          <span className="status-text">F.A.Q</span>
         </div>
       </div>
   );
