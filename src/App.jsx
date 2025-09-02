a58aa7cb6a52ef0d74f608282f8f7166c024895e
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-02 09:53:14 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import TopoMenu from './components/Menu-Topo/NavBar.jsx'
-import BotaoVerde from './components/Simbolos/BotaoVerde.jsx'
-import HeroSection from './components/PaginaPrincipal/HeroSection.jsx'
-import LogosSection from './components/Simbolos/LogosSection.jsx'
-import ServiceSection from './components/CardsService/ServiceSection.jsx'
-import MktSection from './components/MarketplaceSection/MktSection.jsx'
-import WebDesenTexto from './components/WebSection/WebDesen.jsx'
-import SectionContabel from './components/ContabilidadeSection/SectionContabel.jsx'
-import SectionSobre from './components/SobreNos/SectionSobre.jsx'
-import Faq from './components/questions/Faq.jsx'
+import Home from './pages/Home.jsx'
 
 function App() {
   
 
   return (
     <>
-    <TopoMenu/>
-    <BotaoVerde/>
-    <HeroSection/>
-    <LogosSection/>
-    <ServiceSection/>
-    <MktSection/>
-    <WebDesenTexto/>
-    <SectionContabel/>
-    <SectionSobre/>
-    <Faq/>
+    <Home />
     
     
     
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TopoMenu from "../components/Menu-Topo/NavBar.jsx";
+ 
+
+function Home() {
+  return (
+    <>
+        <TopoMenu />
+     
+    </>
+  );
+}   
+
+export default Home;
