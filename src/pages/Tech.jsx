2262209972626df80be32feb954df7c84435dab2
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-10 16:21:13 -0300

    COMIIT

---
+++ src/pages/Tech.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import TopoMenu from "../components/Menu-Topo/NavBar.jsx";
+import BotaoVerde from "../components/Simbolos/BotaoVerde.jsx";
+
+function Tech() {
+  return (
+    <>
+        <TopoMenu />
+        <BotaoVerde />
+        
+     
+    </>
+  );
+}   
+
+export default Tech;
