39c681e3def2f0d43d05f344f0cce36da8ee394f
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-10 16:08:56 -0300

    commit

--- src/components/SobreNos/DireitosSection.jsx
+++ src/components/SobreNos/DireitosSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DireitosSection() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex justify-center items-center w-[1920px] h-[153px] flex-shrink-0">
       <p className="m-0 ml-1 text-center text-base font-normal bg-gradient-to-b from-[#E8E8E8] to-[#939393] bg-clip-text text-transparent">
         © 2025 Grupo Scarlat Digital. Todos os direitos reservados.
       </p>
