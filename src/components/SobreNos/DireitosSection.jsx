8920b6c95c8ea9a9cfdb2a3c7cb7d135ce51f928
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-10 16:06:38 -0300

    commit

--- src/components/SobreNos/DireitosSection.jsx
+++ src/components/SobreNos/DireitosSection.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 function DireitosSection() {
-
-    <p>2025 Grupo Scarlat Digital. Todos os direitos reservados.</p>
+  return (
+    <div className="flex items-center justify-center">
+      <p className="m-0 ml-1 text-center text-base font-normal bg-gradient-to-b from-[#E8E8E8] to-[#939393] bg-clip-text text-transparent">
+        © 2025 Grupo Scarlat Digital. Todos os direitos reservados.
+      </p>
+    </div>
+  );
 }
 
 export default DireitosSection;
