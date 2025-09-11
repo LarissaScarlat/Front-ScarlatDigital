02794731441b2c874affd91130b7a46e7aa36b6a
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-11 18:01:06 -0300

    commit

---
+++ src/components/Programacao/Desenvolvimento.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Desenvolvimento() {
+    return (
+        <div>
+           <h1>Sistemas que trabalham enquanto você cresce</h1>
+        </div>
+    );
+}
+
+export default Desenvolvimento;
+
--- src/pages/Tech.jsx
+++ src/pages/Tech.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import TopoMenu from "../components/Menu-Topo/NavBar.jsx";
 import BotaoVerde from "../components/Simbolos/BotaoVerde.jsx";
+import Desenvolvimento from "../components/Programacao/Desenvolvimento.jsx";
 
 function Tech() {
   return (
     <>
         <TopoMenu />
         <BotaoVerde />
+        <Desenvolvimento />
         
      
     </>
