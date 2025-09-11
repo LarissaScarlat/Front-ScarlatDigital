df30b26aa97191833d170dc9ad7805482b4b792e
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-11 17:15:28 -0300

    commit

---
+++ src/components/SobreNos/ImgSobre.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ImgSobre() {
+  return (
+    <div className="flex items-center">
+      <img 
+        src="sua-imagem.jpg" 
+        alt="Imagem" 
+        className="w-[433px] h-[650px] object-cover rounded-lg " 
+      />
+    </div>
+  );
+}
+
+export default ImgSobre;
--- src/components/SobreNos/SectionSobre.jsx
+++ src/components/SobreNos/SectionSobre.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import SobreIcone from "../Simbolos/Sobre";
-
+import ImgSobre from "./ImgSobre.jsx";  
 
 function SectionSobre() {
   return (
-    <div className="web-desen place-content-start ml-auto justify-self-end px-[390px]">
-      <SobreIcone/>
+    <div className="flex items-start justify-center gap-12 px-[50px]">
+      
+      {/* Imagem do lado esquerdo */}
+      <ImgSobre />
 
-      <article className="text-wrap">
-        <h2>Paixão pela inovação e</h2>
-        <h2>resultado</h2>
-        
-      </article>
+      {/* Textos do lado direito */}
+      <div className="web-desen place-content-start ml-auto justify-self-end px-[390px]">
+        <SobreIcone/>
 
-              <p className="!max-w-[560px] !w-auto !text-left !m-0 mt-2">
-                Somos mais que uma empresa de serviços digitais, <span className="font-bold text-[#9F9F9F]">SOMOS CATALISADORES DE TRANSFORMAÇÃO</span>
-              </p>
-        <br></br>
-        <br></br>
+        <article className="text-wrap">
+          <h2>Paixão pela inovação e</h2>
+          <h2>resultado</h2>
+        </article>
+<br></br>
+        <p className="!max-w-[560px] !w-auto !text-left !m-0 mt-2">
+          Somos mais que uma empresa de serviços digitais, 
+          <span className="font-bold text-[#9F9F9F]"> SOMOS CATALISADORES DE TRANSFORMAÇÃO</span>
+        </p>
+      <br></br>
         <p className="!max-w-[560px] !w-auto !text-left !m-0 mt-6">
           O Grupo Scarlat Digital nasceu da visão de revolucionar como empresas crescem no ambiente digital. 
           Combinamos expertise técnica de ponta com estratégia comercial agressiva, criando 
           um ecossistema completo que acelera resultados e maximiza potencial.
         </p>
-       
-      
+      </div>
     </div>
   );
 }
