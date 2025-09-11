22c5ae385bdf77525de40f8e4b2c2b05b8d5257f
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-11 18:08:43 -0300

    commit

--- src/components/Programacao/Desenvolvimento.jsx
+++ src/components/Programacao/Desenvolvimento.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import ButtonUni from "../Button/ButtonUniversal.jsx";
 
 function Desenvolvimento() {
     return (
         <div>
-           <h1>Sistemas que trabalham enquanto você cresce</h1>
+            <div className="hero-content">
+                <div class="flex justify-center ...">
+                    <article class="text-wrap space-y-16">
+                        <h1>Sistemas que trabalham</h1>
+                        <h1>enquanto voce cresce</h1>
+                    </article>
+                </div>
+            </div>
+            <br></br>
+            
+            <div class="flex justify-center space-y-24 text-center">
+                <p>
+                    Desenvolvimento sob medida de software, aplicações, websites e automações que eliminam gargalos, reduzem custos e aceleram resultados.
+                </p>
+            </div>
+            <br></br>
+            <br></br>
+            <div class="flex justify-center space-y-40">
+                <ButtonUni>Fazer um orçamento</ButtonUni>
+            </div>
         </div>
     );
 }
