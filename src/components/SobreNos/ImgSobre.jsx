47aca39c828f72098d7159272829cfb2efa9f9a5
Larissa Scarlat <Alarissascarlat@gmail.com>
2025-09-11 17:40:57 -0300

    commit

--- src/components/SobreNos/ImgSobre.jsx
+++ src/components/SobreNos/ImgSobre.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import ImgSection from "../../assets/Section/ImgSection.jpg";
 
 function ImgSobre() {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center justify-center">
       <img 
-        src="sua-imagem.jpg" 
-        alt="Imagem" 
-        className="w-[433px] h-[650px] object-cover rounded-lg " 
+        src={ImgSection} 
+        alt="Imagem Sobre Nós" 
+        className="w-[433px] h-[650px] object-cover rounded-lg" 
       />
     </div>
   );
 }
 
 export default ImgSobre;
+
